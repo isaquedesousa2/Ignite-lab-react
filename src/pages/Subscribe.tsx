58384f801f2784778e62329d81fbc3fb1b085ff5
isaquesousa2 <isaqueleal35@gmail.com>
2022-06-27 12:52:40 -0300

    updating image

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,7 @@
 import { FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import imgUrl from "./src/assets/code-mockup.png";
 
 export function Subscribe(){
 
@@ -46,7 +47,7 @@ export function Subscribe(){
                 </div>
            </div>
 
-            <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+            <img src={imgUrl} className="mt-10" alt="" />
         </div>
     )
 }
